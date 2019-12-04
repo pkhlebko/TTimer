5a713f5ea02184639a75ccf30f0199049922b695
Pavel Khlebko <proto@tut.by>
2019-12-04 08:46:02 +0300

    Migration started

---
+++ tt-timer/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TimerComponent } from './timer/timer/timer.component';
+import { SettingsComponent } from './settings/settings/settings.component';
+
+const routes: Routes = [
+  { path: 'timer', component: TimerComponent },
+  { path: 'settings', component: SettingsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ tt-timer/src/app/timer/time-counter/time-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-counter',
+  templateUrl: './time-counter.component.html',
+  styleUrls: ['./time-counter.component.scss']
+})
+export class TimeCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
