8a4abdf45813b69f45f8b801543c13c67a67fb59
Pavel Khlebko <pavel.khlebko@clarivate.com>
2020-01-27 17:51:36 +0300

    Added landing

--- tt-timer/src/app/app-routing.module.ts
+++ tt-timer/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TimerComponent } from './timer/pages/timer/timer.component';
-import { SettingsComponent } from './settings/settings/settings.component';
+import { SettingsComponent } from './settings/components/settings/settings.component';
+import { LandingComponent } from './landing/components/landing/landing.component';
 
 const routes: Routes = [
+  { path: 'landing', component: LandingComponent },
   { path: 'timer', component: TimerComponent },
   { path: 'settings', component: SettingsComponent}
 ];
--- tt-timer/src/app/app.module.ts
+++ tt-timer/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SettingsModule } from './settings/settings.module';
 import { TimerModule } from './timer/timer.module';
+import { LandingComponent } from './landing/components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingComponent,
   ],
   imports: [
     AppRoutingModule,
